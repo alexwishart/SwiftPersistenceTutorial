0d5fc2d66ce21a1c0020dca7b91047dbd44e3e23
andy <andrew.lees11@ibm.com>
2017-11-13 15:51:47 +0000

    removed persistance sections

--- FoodServer/Sources/Application/Application.swift
+++ FoodServer/Sources/Application/Application.swift
@@ -5,29 +5,16 @@ import Configuration
 import CloudEnvironment
 import KituraContracts
 import Health
-import SwiftKuery
-import SwiftKueryPostgreSQL
 
 public let projectPath = ConfigurationManager.BasePath.project.path
 public let health = Health()
 
-public class Meals : Table {
-    let tableName = "meals"
-    let key = Column("key")
-    let name = Column("name")
-    let photo = Column("photo")
-    let rating = Column("rating")
-}
-
 public class App {
     let router = Router()
     let cloudEnv = CloudEnv()
-    let meals = Meals()
-    let connection = PostgreSQLConnection(host: "localhost", port: 5432, options: [.databaseName("FoodDatabase")])
     private var mealStore: [String: Meal] = [:]
     
     public init() throws {
-
     }
 
     func postInit() throws {
@@ -40,72 +27,15 @@ public class App {
         router.post("/meals", handler: storeHandler)
         router.get("/meals", handler: loadHandler)
     }
-    
-    func storeHandler(meal: Meal, completion: @escaping (Meal?, RequestError?) -> Void ) -> Void {
-        connection.connect() { error in
-            if let error = error {
-                print("Error is \(error)")
-                return
-            }
-            else {
-                let key = String(arc4random())
-                let updateQuery = Update(meals, set: [(meals.key, key), (meals.name, meal.name), (meals.photo, "test"),(meals.rating, meal.rating)]).where(meals.name == meal.name)
-                print("meals: \(meals.name) + \(meal.name)")
-                    connection.execute(query: updateQuery) { result in
-                        print("update result: \(String(describing: result))")
-                            let insertQuery = Insert(into: self.meals, values: [key, meal.name, "test", meal.rating])
-                            self.connection.execute(query: insertQuery) { result in
-                                print("insert result: \(result.success)")
-                                if(!result.success){
-                                    completion(nil, .unprocessableEntity)
-                                    return
-                                }
-                                print("inserted to table")
-                            }
-                        
-            }
-        completion(meal, nil)
-            }
-        }
+
+    func storeHandler(meal: Meal, completion: (Meal?, RequestError?) -> Void ) -> Void {
+        mealStore[meal.name] = meal
+        completion(mealStore[meal.name], nil)
     }
     
-    func loadHandler(completion: @escaping ([Meal]?, RequestError?) -> Void ) -> Void {
-        print("entered loadhandler")
-        var tempMealStore: [String: Meal] = [:]
-        connection.connect() { error in
-            if let error = error {
-                print("Error is \(error)")
-                return
-            }
-            else {
-                let query = Select(from :meals)
-                connection.execute(query: query) { queryResult in
-                    print(queryResult)
-                    if let resultSet = queryResult.asResultSet {
-                        print("result set")
-                        for row in resultSet.rows {
-                            print("inside rows")
-                            guard let name = row[1], let nameString = name as? String else{
-                                return
-                            }
-                            guard let photo = row[2], let photoString = photo as? String   else{return}
-                            var photoArray = [String]()
-                            photoArray.append(photoString)
-                            guard let photoData = try? JSONSerialization.data(withJSONObject: photoArray, options: []) else {return}
-                            guard let rating = row[3], let ratingInt = Int(String(describing: rating)) else{return}
-                            let currentMeal = Meal(name: nameString, photo: photoData, rating: ratingInt)
-                            tempMealStore[nameString] = currentMeal
-                        }
-                    }
-                    else{
-                        print("queryResult error: " )
-                    }
-                }
-            }
-        }
-        self.mealStore = tempMealStore
-        let returnMeals: [Meal] = self.mealStore.map({ $0.value })
-        completion(returnMeals, nil)
+    func loadHandler(completion: ([Meal]?, RequestError?) -> Void ) -> Void {
+        let meals: [Meal] = self.mealStore.map({ $0.value })
+        completion(meals, nil)
     }
     
     public func run() throws {
